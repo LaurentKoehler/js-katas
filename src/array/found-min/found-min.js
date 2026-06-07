b608b429482199d11a9d9001a823949064041321
Laurent Koehler <laurent.koehler@gmail.com>
2026-06-07 17:53:38 +0200

    found-min

--- src/array/found-min/found-min.js
+++ src/array/found-min/found-min.js
@@ -13,5 +13,20 @@ You can't use the function Math.min()
 */
 
 // TODO add your code here
+function min(array) {
+    if (array === null || array.length === 0) {
+        return null
+    };
 
+    let minNumber = array[0];
+
+    for (const number of array) {
+        if (number < minNumber) {
+            minNumber = number
+        };
+    }
+    return minNumber;
+
+
+}
 module.exports = min;
